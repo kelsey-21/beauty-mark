b3e51ac5aa0cc2c7949fb9d8a70adc0d001a156c
Kelsey Creel <kelseycreel@gmail.com>
2020-01-21 19:27:15 -0600

    routing complete

---
+++ src/components/pages/ProductForm/ProductForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './ProductForm.scss';
+
+class ProductForm extends React.Component {
+  render() {
+    return (
+      <div className="ProductForm">
+        <h1>ProductForm Page</h1>
+      </div>
+    );
+  }
+}
+
+export default ProductForm;
