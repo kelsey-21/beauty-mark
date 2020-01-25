244e39fe737978f9f74bb7085cf4eb8bf9af4350
Kelsey Creel <kelseycreel@gmail.com>
2020-01-25 09:31:03 -0600

    search complete

--- src/components/shared/SearchCard/SearchCard.js
+++ src/components/shared/SearchCard/SearchCard.js
@@ -5,12 +5,23 @@ import {
 } from 'reactstrap';
 
 import shapes from '../../../helpers/props/shapes';
+import authData from '../../../helpers/data/authData';
 
 import './SearchCard.scss';
 
 class SearchCard extends React.Component {
   static propTypes = {
     product: shapes.productShape,
+  }
+
+  saveProductEvent = (e) => {
+    e.preventDefault();
+    const { product } = this.props;
+    const uid = authData.getUid();
+    const newUserProduct = {};
+    newUserProduct.productId = product.id;
+    newUserProduct.uid = uid;
+    console.log(newUserProduct);
   }
 
   render() {
@@ -24,7 +35,7 @@ class SearchCard extends React.Component {
           <CardTitle>{product.name}</CardTitle>
           <CardSubtitle>{product.brand} {product.category}</CardSubtitle>
           <CardText>Ingredients: {product.ingredients}</CardText>
-          <Button>Add to my bag</Button>
+          <Button onClick={this.saveProductEvent}>Add to my bag</Button>
         </CardBody>
       </Card>
     </div>
