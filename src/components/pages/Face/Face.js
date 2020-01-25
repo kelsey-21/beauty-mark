36e94c278962e99bbea351ff33e282ba70a97d5a
Kelsey Creel <kelseycreel@gmail.com>
2020-01-24 21:10:55 -0600

    WIP

--- src/components/pages/Face/Face.js
+++ src/components/pages/Face/Face.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import SingleLine from '../../shared/SingleLine/SingleLine';
 import productData from '../../../helpers/data/productData';
+import image from '../../../helpers/images/background.png';
 
 import './Face.scss';
 
@@ -20,7 +21,8 @@ class Face extends React.Component {
     const singleLineDiv = categories.map((category) => <SingleLine key={category.id} category={category.category} />);
     return (
       <div className="Face">
-        <div className="face-image">
+        <img className="face-image" src={image} alt="face" />
+        <div>
           {singleLineDiv}
         </div>
       </div>
