433c6d9967ce413b3a5adf6e825d5af874ad0ecd
Kelsey Creel <kelseycreel@gmail.com>
2020-01-29 13:25:08 -0600

    WIP

--- src/helpers/data/smash.js
+++ src/helpers/data/smash.js
@@ -23,36 +23,102 @@ const getCompleteUserProducts = () => new Promise((resolve, reject) => {
     .catch((error) => reject(error));
 });
 
-const getProductsWithIngredients = () => new Promise((resolve, reject) => {
-  const products = [];
-  productData.getAllProducts()
-    .then((allProducts) => {
-      allProducts.forEach((product) => {
-        const ingredients = product.ingredients.split(', ');
-        const newProduct = { ...product };
-        newProduct.ingredientArr = ingredients;
-        products.push(newProduct);
-      });
-      resolve(products);
-    })
-    .catch((error) => reject(error));
-});
-
 const getProductRisks = () => new Promise((resolve, reject) => {
   productData.getAllProducts()
     .then((allProducts) => {
+      const productRisks = [];
       learnData.getAllLearns()
         .then((risks) => {
           allProducts.forEach((product) => {
             const ingredientsArr = product.ingredients.split(', ');
             ingredientsArr.forEach((ingredient) => {
-              const matching = risks.find((risk) => risk.name === ingredient);
-              console.log(matching);
+              const ingredientLC = ingredient.toLowerCase();
+              const matching = risks.find((risk) => risk.name.toLowerCase() === ingredientLC);
+              if (!matching) {
+                if (ingredientLC.includes('ci ')) {
+                  const productRiskObj = {};
+                  productRiskObj.productid = product.id;
+                  productRiskObj.riskId = 'risk8';
+                  productRisks.push(productRiskObj);
+                } if (ingredientLC.match(/\wparaben/g)) {
+                  const productRiskObj = {};
+                  productRiskObj.productid = product.id;
+                  productRiskObj.riskId = 'risk4';
+                  productRisks.push(productRiskObj);
+                } if (ingredientLC.includes('polyethylene')) {
+                  const productRiskObj = {};
+                  productRiskObj.productid = product.id;
+                  productRiskObj.riskId = 'risk3';
+                  productRisks.push(productRiskObj);
+                } if (ingredientLC.includes('hydroxyanisole')) {
+                  const productRiskObj = {};
+                  productRiskObj.productid = product.id;
+                  productRiskObj.riskId = 'risk5';
+                  productRisks.push(productRiskObj);
+                } if (ingredientLC.includes('hydroxytoluene')) {
+                  const productRiskObj = {};
+                  productRiskObj.productid = product.id;
+                  productRiskObj.riskId = 'risk6';
+                  productRisks.push(productRiskObj);
+                } if (ingredientLC.includes('hydroxytoluene')) {
+                  const productRiskObj = {};
+                  productRiskObj.productid = product.id;
+                  productRiskObj.riskId = 'risk6';
+                  productRisks.push(productRiskObj);
+                }
+              }
             });
           });
+          console.log(productRisks);
         });
     })
     .catch((error) => reject(error));
 });
 
-export default { getCompleteUserProducts, getProductRisks };
+const matchProductRisks = (product) => {
+  const ingredientsArr = product.ingredients.split(', ');
+  const productRisks = [];
+  learnData.getAllLearns()
+    .then((risks) => {
+      ingredientsArr.forEach((ingredient) => {
+        const ingredientLC = ingredient.toLowerCase();
+        const matching = risks.find((risk) => risk.name.toLowerCase() === ingredientLC);
+        if (!matching) {
+          if (ingredientLC.includes('ci ')) {
+            const productRiskObj = {};
+            productRiskObj.productid = product.id;
+            productRiskObj.riskId = 'risk8';
+            productRisks.push(productRiskObj);
+          } if (ingredientLC.match(/\wparaben/g)) {
+            const productRiskObj = {};
+            productRiskObj.productid = product.id;
+            productRiskObj.riskId = 'risk4';
+            productRisks.push(productRiskObj);
+          } if (ingredientLC.includes('polyethylene')) {
+            const productRiskObj = {};
+            productRiskObj.productid = product.id;
+            productRiskObj.riskId = 'risk3';
+            productRisks.push(productRiskObj);
+          } if (ingredientLC.includes('hydroxyanisole')) {
+            const productRiskObj = {};
+            productRiskObj.productid = product.id;
+            productRiskObj.riskId = 'risk5';
+            productRisks.push(productRiskObj);
+          } if (ingredientLC.includes('hydroxytoluene')) {
+            const productRiskObj = {};
+            productRiskObj.productid = product.id;
+            productRiskObj.riskId = 'risk6';
+            productRisks.push(productRiskObj);
+          } if (ingredientLC.includes('hydroxytoluene')) {
+            const productRiskObj = {};
+            productRiskObj.productid = product.id;
+            productRiskObj.riskId = 'risk6';
+            productRisks.push(productRiskObj);
+          }
+        }
+      });
+    })
+    .catch((error) => console.error(error));
+};
+
+export default { getCompleteUserProducts, getProductRisks, matchProductRisks };
